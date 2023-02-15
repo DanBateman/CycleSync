5c1c821e900da63376157164ecbcfe6de6788dfe
Daniel Bateman <105228059+DanBateman@users.noreply.github.com>
2023-02-15 16:12:58 -0700

    Converted to latest react-dom syntax

--- client/src/index.js
+++ client/src/index.js
@@ -1,13 +1,14 @@
 import React, { StrictMode } from "react";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
+import config from "./config";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(
   <StrictMode>
-    <BrowserRouter basename="localhost:3000">
+    <BrowserRouter basename={config.basePath}>
       <App />
     </BrowserRouter>
-  </StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
