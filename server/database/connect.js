f43b533df2b045e19bddf3ae02ec781bab8d80f0
Daniel Bateman <105228059+DanBateman@users.noreply.github.com>
2023-03-01 14:06:18 -0700

    Created connection file for mongdDB

---
+++ server/database/connect.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const connect = async () => {
+  try {
+    const options = {
+      useNewUrlParser: true,
+    };
+
+    const connection = await mongoose.connect(
+      "mongodb://user:pass@127.0.0.1:1512/",
+      options
+    );
+    if (connection)
+      console.log("\x1b[32m%s\x1b[0m", "Database Connected Successfully...");
+    return connection;
+  } catch (err) {
+    console.log("\x1b[31m%s\x1b[0m", "Error while connecting database\n");
+    console.log(err);
+  }
+};
+const conn = connect();
+module.exports = conn;
