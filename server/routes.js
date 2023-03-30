e15601b29dcc4cd580341250ef802b283479e544
DanBateman <105228059+DanBateman@users.noreply.github.com>
2023-03-30 14:54:10 -0600

    Added user route

--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 
 router.use("/calendar", require("./calendar/calendarRoutes"));
+router.use("/user", require("./user/userRoutes"));
 
 module.exports = router;
