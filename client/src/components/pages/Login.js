86af11052ade04bbb18fd8d43a6f29435117b2ec
Daniel Bateman <105228059+DanBateman@users.noreply.github.com>
2023-04-04 13:01:03 -0600

    Login page styling

--- client/src/components/pages/Login.js
+++ client/src/components/pages/Login.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { useState } from "react";
+import { Redirect, useHistory } from "react-router-dom";
 import { hashSync } from "bcryptjs";
 import {
   Paper,
@@ -9,17 +10,25 @@ import {
   FormControl,
   Button,
   TextField,
+  InputAdornment,
 } from "@mui/material";
-import { useDispatch } from "react-redux";
+import Visibility from "@mui/icons-material/Visibility";
+import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import { useDispatch, useSelector } from "react-redux";
 import { loginThunk } from "../auth/authSlice";
 
 const LoginPage = () => {
   const [loginUsername, setLoginUsername] = useState("");
   const [loginPassword, setLoginPassword] = useState("");
+  const [showLoginPass, setShowLoginPass] = useState(false);
   const [signUpEmail, setSignUpEmail] = useState("");
   const [signUpUsername, setSignUpUsername] = useState("");
   const [signUpPassword, setSignUpPassword] = useState("");
+  const [showSignUpPass, setShowSignUpPass] = useState(false);
+  const [showConfirmPass, setShowConfirmPass] = useState(false);
   const [confirmPassword, setConfirmPassword] = useState("");
+  const [login, setLogin] = useState(true);
+  const error = useSelector((state) => state.auth.error);
   const dispatch = useDispatch();
 
   const sendLogin = () => {
@@ -31,113 +40,209 @@ const LoginPage = () => {
         pass: hashedPassword,
       })
     );
+    if (!error) {
+      return <Redirect to={"/calendar"} />;
+    } else {
+    }
   };
 
   return (
     <Paper
+      elevation={8}
       sx={{
-        width: "fit-content",
-        height: "fit-content",
+        width: "50%",
+        minWidth: "700px",
+        height: "600px",
         mx: "auto",
         my: "200px",
+        display: "flex",
       }}
     >
       <Box
         sx={{
-          width: "fit-content",
-          height: "auto",
-          display: "flex",
-          flexDirection: "column",
+          width: "49%",
+          backgroundImage: "radial-gradient(circle, white, #FAA)",
         }}
-      >
-        <Typography variant="h5" sx={{ mx: 2.7 }}>
-          Login
-        </Typography>
-        <FormControl sx={{ mx: 2, my: 1 }}>
-          <TextField
-            variant="outlined"
-            label={"Username"}
-            value={loginUsername}
-            onChange={(e) => {
-              setLoginUsername(e.target.value);
-            }}
-          />
-        </FormControl>
-        <FormControl sx={{ mx: 2, my: 1 }}>
-          <TextField
-            variant="outlined"
-            label={"Password"}
-            value={loginPassword}
-            onChange={(e) => {
-              setLoginPassword(e.target.value);
-            }}
-          />
-        </FormControl>
-        <Box
-          sx={{ display: "flex", flexDirection: "row-reverse", mx: 2, my: 1 }}
-        >
-          <Button variant="contained" onClick={sendLogin}>
-            Login
-          </Button>
-        </Box>
-      </Box>
-      <Divider variant="middle" />
+      />
+      <Divider orientation="vertical" />
       <Box
         sx={{
-          width: "fit-content",
-          height: "auto",
+          width: "49%",
+          height: "100%",
           display: "flex",
           flexDirection: "column",
+          justifyContent: "center",
         }}
       >
-        <Typography variant="h5" sx={{ mx: 2.7 }}>
-          Sign Up
-        </Typography>
-        <FormControl sx={{ mx: 2, my: 1 }}>
-          <TextField
-            variant="outlined"
-            label={"Email"}
-            value={signUpEmail}
-            onChange={(e) => {
-              setSignUpEmail(e.target.value);
+        {login && (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              mx: "15%",
             }}
-          />
-        </FormControl>
-        <FormControl sx={{ mx: 2, my: 1 }}>
-          <TextField
-            variant="outlined"
-            label={"Username"}
-            value={signUpUsername}
-            onChange={(e) => {
-              setSignUpUsername(e.target.value);
+          >
+            <Typography variant="h5" sx={{ mx: 2.7, my: 1 }} align="center">
+              Login
+            </Typography>
+            <FormControl sx={{ mx: 2, my: 1 }}>
+              <TextField
+                variant="outlined"
+                label={"Username"}
+                value={loginUsername}
+                onChange={(e) => {
+                  setLoginUsername(e.target.value);
+                }}
+              />
+            </FormControl>
+            <FormControl sx={{ mx: 2, my: 1 }}>
+              <TextField
+                variant="outlined"
+                label={"Password"}
+                type={showLoginPass ? "text" : "password"}
+                value={loginPassword}
+                onChange={(e) => {
+                  setLoginPassword(e.target.value);
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      {showLoginPass ? (
+                        <VisibilityOff
+                          sx={{
+                            cursor: "pointer",
+                          }}
+                          onClick={() =>
+                            setShowLoginPass((current) => !current)
+                          }
+                        />
+                      ) : (
+                        <Visibility
+                          sx={{
+                            cursor: "pointer",
+                          }}
+                          onClick={() =>
+                            setShowLoginPass((current) => !current)
+                          }
+                        />
+                      )}
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </FormControl>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row-reverse",
+                justifyContent: "space-between",
+                mx: 2,
+                my: 1,
+              }}
+            >
+              <Button variant="contained" onClick={sendLogin}>
+                Login
+              </Button>
+              <Button onClick={() => setLogin((current) => !current)}>
+                {login ? "Sign Up" : "Login"}
+              </Button>
+            </Box>
+          </Box>
+        )}
+        {!login && (
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              mx: "15%",
             }}
-          />
-        </FormControl>
-        <FormControl sx={{ mx: 2, my: 1 }}>
-          <TextField
-            variant="outlined"
-            label={"Password"}
-            value={signUpPassword}
-            onChange={(e) => {
-              setSignUpPassword(e.target.value);
-            }}
-          />
-        </FormControl>
-        <FormControl sx={{ mx: 2, my: 1 }}>
-          <TextField
-            variant="outlined"
-            label={"Confirm Password"}
-            value={confirmPassword}
-            onChange={(e) => {
-              setConfirmPassword(e.target.value);
-            }}
-          />
-        </FormControl>
-        <Box
-          sx={{ display: "flex", flexDirection: "row-reverse", mx: 2, my: 1 }}
-        >
-          <Button variant="contained">Sign Up</Button>
-        </Box>
+          >
+            <Typography variant="h5" sx={{ mx: 2.7, my: 2 }} align="center">
+              Sign Up
+            </Typography>
+            <FormControl sx={{ mx: 2, my: 1 }}>
+              <TextField
+                variant="outlined"
+                label={"Email"}
+                value={signUpEmail}
+                onChange={(e) => {
+                  setSignUpEmail(e.target.value);
+                }}
+              />
+            </FormControl>
+            <FormControl sx={{ mx: 2, my: 1 }}>
+              <TextField
+                variant="outlined"
+                label={"Username"}
+                value={signUpUsername}
+                onChange={(e) => {
+                  setSignUpUsername(e.target.value);
+                }}
+              />
+            </FormControl>
+            <FormControl sx={{ mx: 2, my: 1 }}>
+              <TextField
+                variant="outlined"
+                label={"Password"}
+                type={showSignUpPass ? "text" : "password"}
+                value={signUpPassword}
+                onChange={(e) => {
+                  setSignUpPassword(e.target.value);
+                }}
+                InputProps={{
+                  endAdornment: (
+                    <InputAdornment position="end">
+                      {showSignUpPass ? (
+                        <VisibilityOff
+                          sx={{
+                            cursor: "pointer",
+                          }}
+                          onClick={() =>
+                            setShowSignUpPass((current) => !current)
+                          }
+                        />
+                      ) : (
+                        <Visibility
+                          sx={{
+                            cursor: "pointer",
+                          }}
+                          onClick={() =>
+                            setShowSignUpPass((current) => !current)
+                          }
+                        />
+                      )}
+                    </InputAdornment>
+                  ),
+                }}
+              />
+            </FormControl>
+            <FormControl sx={{ mx: 2, my: 1 }}>
+              <TextField
+                variant="outlined"
+                label={"Confirm Password"}
+                type="password"
+                value={confirmPassword}
+                onChange={(e) => {
+                  setConfirmPassword(e.target.value);
+                }}
+              />
+            </FormControl>
+            <Box
+              sx={{
+                display: "flex",
+                flexDirection: "row-reverse",
+                justifyContent: "space-between",
+                mx: 2,
+                my: 1,
+              }}
+            >
+              <Button variant="contained">Sign Up</Button>
+              <Button onClick={() => setLogin((current) => !current)}>
+                {login ? "Sign Up" : "Login"}
+              </Button>
+            </Box>
+          </Box>
+        )}
       </Box>
     </Paper>
   );
